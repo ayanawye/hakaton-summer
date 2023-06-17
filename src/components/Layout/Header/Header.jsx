a47e651a316050372ab466188d593d197dd8e8d5
bayellaisss <119422574+bayellaisss@users.noreply.github.com>
2023-06-17 13:12:54 +0600

    header

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import s from "./Header.module.scss"
 
-const Header = () => {
+function Header() {
   return (
-    <div className={s.test}>
-      header
-    </div>
+    <header className='header'>
+      <div className={s.container}>
+        <div>
+        <h1>Заголовок</h1>
+        <nav className="nav">
+          <ul>
+            <li className='aboutus'><a href="#">О нас</a></li>
+            <li className='test'><a href="#">Tect</a></li>
+          </ul>
+      </nav>
+        </div>
+      </div>
+    </header>
   );
-};
+}
 
 export default Header;
